123629b94cad123e0fc18562ca334697a930a113
Jay Kishan <findmejay24@gmail.com>
2025-10-12 08:49:23 +0530

    Added functionality to route /

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const Listing = require("./models/listing.js");
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
@@ -78,8 +79,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/", (req, res) => {
-  res.render("listings/index.ejs");
+app.use("/", async(req, res) => {
+  let allListings = await Listing.find();
+  res.render("listings/index.ejs", { allListings });
 });
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
