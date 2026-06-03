89bcb9796f9087347d0643717b87426e037b286e
Jay Kishan <findmejay24@gmail.com>
2026-06-03 13:34:42 +0530

    update wanderlust

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ const store = Mongostore.create({
   touchAfter: 24 * 3600,
 });
 
-store.on("error", () => {
+store.on("error", (err) => {
   console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
